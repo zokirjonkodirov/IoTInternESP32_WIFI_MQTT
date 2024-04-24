5128ef3ff79550778ba426cf44f5d5b5c4d8b46a
Maqsud Usmonov <maqsudbek94@gmail.com>
2024-04-24 14:22:56 +0500

    added some comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,37 @@
 
 #include <Arduino.h>
 
+// Include necessary libraries here
+// TODO: Add necessary libraries here
+
+// Define global variables here
+// TODO: Add global variables here
+
+// Define functions here
+// TODO: Add functions here
+
+/**********************************/
+// SETUP //
 void setup(void) 
 {
+  // Initialize peripherals
+  // TODO: Initialize peripherals here
+  // TODO: LEDS, Buttons, LCD, etc.
+
+  // Connect to WiFi
+  // TODO: Connect to WiFi here
+
+  // Setup MQTT
+  // TODO: Setup MQTT here
+
+  // 
 
 }
 
+/**********************************/
+// LOOP //
 void loop(void) 
 {
-  // put your main code here, to run repeatedly:
+  // Keep WiFi and MQTT connection alive
+  // TODO: Keep WiFi and MQTT connection alive here
 }
