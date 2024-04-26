ead7304d392fde3b81d3a312d476d4d57f1788af
Maqsud Usmonov <maqsudbek94@gmail.com>
2024-04-26 12:23:51 +0500

    Added necessary libraries and updated WiFi and MQTT credentials in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <Arduino.h>
 
 // Include necessary libraries here
-// TODO: Add necessary libraries here
+
 #include <WiFi.h>
 #include <PubSubClient.h>
 #include <LiquidCrystal_I2C.h>
@@ -10,8 +10,29 @@
 
 
 // Define global variables here
-// TODO: Add global variables here
 
+// WiFi credentials
+const char *ssid = "IoTExperiment";
+const char *password = "iotexperiment303ab";
+
+// MQTT credentials
+const char *mqtt_server = "mqtt.iotserver.uz";
+const int mqtt_port = 1883;
+const char *mqtt_user = "userTTPU";
+const char *mqtt_password = "mqttpass";
+
+// MQTT client unique ID
+const char *mqtt_client_id = "ttpuIoTTEacherEsp32";
+
+// MQTT topics
+const char *globalLedTopic = "ttpu/iot/global/led";
+const char *btnPressTopic = "ttpu/iot/teacher/btn";
+
+// pinout
+const int redPin = 32;
+const int yellowPin = 33;
+const int greenPin = 25;
+const int buttonPin = 26;
 
 // Define functions here
 // TODO: Add functions here
